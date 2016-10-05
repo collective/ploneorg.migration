d2e6cdc78df68da5a08b0373fe33c5c03fb21b9e
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-10-05 14:19:36 +0200

    Add missing export script

--- src/ploneorg/migration/usermigration.py
+++ src/ploneorg/migration/usermigration.py
@@ -2,6 +2,17 @@ import json
 import transaction
 
 
+def export_userdb(site):
+    """ Exports all the users and passwords to a file """
+    pas = site.acl_users
+    users = pas.source_users
+    thefile = open('userdb.txt', 'w')
+    passwords = users._user_passwords
+    result = dict(passwords)
+    thefile.write(result)
+    thefile.close()
+
+
 def import_userdb(site):
     pas = site.acl_users
     users = pas.source_users
